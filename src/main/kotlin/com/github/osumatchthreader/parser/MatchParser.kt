92dc4b8cd80c9519c785b374eeffe78917b72cef
osuMatchThreader <osumatchthreader@gmail.com>
2016-11-12 16:07:59 +0100

    horrible hacks LUL
    team_scores are incomplete when everybody on a team fails

--- src/main/kotlin/com/github/osumatchthreader/parser/MatchParser.kt
+++ src/main/kotlin/com/github/osumatchthreader/parser/MatchParser.kt
@@ -4,6 +4,8 @@ import com.github.osumatchthreader.log.Log
 import com.github.osumatchthreader.log.entering
 import com.github.osumatchthreader.log.exitWith
 import com.github.osumatchthreader.model.Match
+import com.github.osumatchthreader.model.Team
+import com.github.osumatchthreader.model.TeamScores
 import org.jsoup.nodes.Element
 
 class MatchParser(val beatmapParser: BeatmapParser,
@@ -33,7 +35,21 @@ class MatchParser(val beatmapParser: BeatmapParser,
     val playerScores = e.select("tr.row1p, tr.row2p").map {
       matchScoreParser.parse(it)
     }
-    val teamScores = e.select("div.team_scores").first().let { teamScoresParser.parse(it) }
+
+    val teamScores = try {
+      e.select("div.team_scores").first().let { teamScoresParser.parse(it) }
+    } catch(e: Exception) {
+      fun teamScore(team: Team) = playerScores
+          .filter { it.team == team }
+          .filter { !it.failed }
+          .map { it.score }
+          .fold(0L, Long::plus)
+
+      val bts = teamScore(Team.BLUE)
+      val rts = teamScore(Team.RED)
+      val winner = if (bts > rts) Team.BLUE else Team.RED
+      TeamScores(bts, rts, winner)
+    }
 
     return log.exitWith(Match(beatmap, gameName, duration, gameMode, scoringMode, playerScores, teamScores))
   }
